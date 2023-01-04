ee113dac489bd76052d351fa0f1b68656cfb2c37
mzparulina <pauline.revilla@gmail.com>
2023-01-04 18:49:34 -0500

    added play.js

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("you are connected");
+  });
+
+  conn.on("data", () => {
+    console.log("i am data");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
