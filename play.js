7183eba6f7ebc5254e4339e1c4878e7ac75cd8f8
mzparulina <pauline.revilla@gmail.com>
2023-01-05 13:18:19 -0500

    renamed setupinput to input

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const { connect } = require('./client');
-const { setupInput } = require('./setupInput');
+const { setupInput } = require('./input');
 
 
 console.log("Connecting ...");
-const conn = connect();
-setupInput(conn);
+connect();
+setupInput();
